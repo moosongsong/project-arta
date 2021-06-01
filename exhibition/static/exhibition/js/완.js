b6eb1b5dacd7dcc566a2fc070838782ea97ac9d8
moosongsong <songe08@gmail.com>
2021-06-01 19:28:40 +0900

    프로젝트 기말 발표이후 코드 정리

--- exhibition/static/exhibition/js/완.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.borderRight = "2px solid #5CA4A2";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.borderRight = "none";
-}
