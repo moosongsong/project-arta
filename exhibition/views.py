7fed58f2caf319ef196067fde91f1a1640a46fb7
moosongsong <songe08@gmail.com>
2021-07-02 12:04:02 +0900

    [Fixed] 전시자 전시회 목록 전시 예정 구간 추가

--- exhibition/views.py
+++ exhibition/views.py
@@ -347,15 +347,17 @@ class ExhibitionListForArtist(ListView):
         if open_mode == 'all':
             exhibition_list = Exhibition.objects.filter(user=self.request.user).order_by('end_at')
         elif open_mode == 'open':
-            exhibition_list = Exhibition.objects.filter(user=self.request.user, end_at__gt=datetime.datetime.now()).order_by('end_at')
+            exhibition_list = Exhibition.objects.filter(user=self.request.user, end_at__gte=datetime.datetime.now(), start_at__lte=datetime.datetime.now()).order_by('end_at')
         elif open_mode == 'close':
-            exhibition_list = Exhibition.objects.filter(user=self.request.user, end_at__lte=datetime.datetime.now()).order_by('end_at')
+            exhibition_list = Exhibition.objects.filter(user=self.request.user, end_at__lt=datetime.datetime.now()).order_by('end_at')
+        elif open_mode == 'ready':
+            exhibition_list = Exhibition.objects.filter(user=self.request.user, start_at__gt=datetime.datetime.now()).order_by('end_at')
         return exhibition_list
 
     def get_context_data(self, **kwargs):
         open_mode = self.kwargs['pk']
         context = super(ExhibitionListForArtist, self).get_context_data()
-        if open_mode == 'all' or open_mode == 'open' or open_mode == 'close':
+        if open_mode == 'all' or open_mode == 'open' or open_mode == 'close' or open_mode == 'ready':
             context['category_name'] = open_mode
         else:
             context['category_name'] = 'all'
