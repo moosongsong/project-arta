e30f8eddf704959d18f49e8a3a97aa449048e5a3
moosongsong <songe08@gmail.com>
2021-07-07 15:11:09 +0900

    [Fixed] main.js 로 분리

---
+++ exhibition/static/exhibition/js/main.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//     function message_alert(title, contents) {
+//         swal(title, contents);
+//     }
+// });
+
+function message_alert(title, contents) {
+    swal(title, contents);
+}
+
+function openNav() {
+    var mql = window.matchMedia("screen and (min-width: 992px)");
+
+    if (mql.matches) {
+        document.getElementById("mySidenav").style.width = "300px";
+    } else {
+        document.getElementById("mySidenav").style.width = "180px";
+    }
+
+    document.getElementById("mySidenav").style.borderRight = "2px solid #5CA4A2";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.borderRight = "none";
+}
